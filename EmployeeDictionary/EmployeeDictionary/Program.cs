2c3426263a4586aa2487291b4235ae515a7ecfd0
iwujif03 <37908333+iwujif03@users.noreply.github.com>
2018-03-29 11:11:30 -0500

    First Commit
    Testing App

--- EmployeeDictionary/EmployeeDictionary/Program.cs
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -10,6 +10,20 @@ namespace EmployeeDictionary
     {
         static void Main(string[] args)
         {
+            var Employee = new Dictionary<int,TheEmployee>();
+
+            Employee.Add(101,
+                new TheEmployee { Id = 101, FirstName = "Jesse", LastName = " Liberty" });
+            Employee.Add(102,
+                new TheEmployee { Id = 102, FirstName = "George", LastName = "Washington" });
+            Employee.Add(103,
+                new TheEmployee { Id = 103, FirstName = "Mark", LastName = " Smith" });
+           
+                
+            Console.WriteLine(Employee[101]);
+            Console.WriteLine(Employee[102]);
+            Console.WriteLine(Employee[103]);
+            Console.ReadLine();
         }
     }
 }
--- EmployeeDictionary/EmployeeDictionary/TheEmployee.cs
+++ EmployeeDictionary/EmployeeDictionary/TheEmployee.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace EmployeeDictionary
 {
     class TheEmployee
+
     {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public int Id { get; set; }
+        public override string ToString()
+        {
+            return Id.ToString() + "," +
+                FirstName + " " + LastName;
+        }
     }
 }
